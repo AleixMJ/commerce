231bb32fe61610c5ed4ef82bf5da9339d003c605
AleixMJ <aleixtechIT@gmail.com>
2022-12-21 19:04:42 +0000

    added def__str__(self) for all models

--- auctions/models.py
+++ auctions/models.py
@@ -9,20 +9,32 @@ class Auction(models.Model):
     title = models.CharField(max_length=64)
     text = models.TextField()
     url = models.URLField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
     category = models.CharField(max_length=64, default='uncategorised')
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.id}: Auction {self.title} from {self.user}"
 
 class Bid(models.Model):
     item = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name='bids')
     price = models.FloatField()
     bidder = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.id}: {self.price} bid for Auction {self.item} from {self.bidder}"
 
-class Comments(models.Model):
+class Comment(models.Model):
     item = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.id}: comment for Auction {self.item} from {self.user}"
+
 class Category(models.Model):
     title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
